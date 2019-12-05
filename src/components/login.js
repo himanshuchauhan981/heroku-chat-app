ad578039fbd7ec3a507d50a4c181b23c0e4d8f4a
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2019-12-05 10:56:19 +0530

    login routes and socket url changed

--- src/components/login.js
+++ src/components/login.js
@@ -29,8 +29,13 @@ class Login extends Component{
             status = true
         }
         if(status){
-            axios.get('/login/validate',{
-                params : obj
+            axios({
+                method:'get',
+                url:'https://smart-chat-backend.herokuapp.com/login/validate',
+                params : obj,
+                headers : {
+                    'Content-Type': 'application/json'
+                }
             })
             .then((response)=>{
                 if(response.data.validate){
@@ -51,9 +56,16 @@ class Login extends Component{
         let loginpassword = event.target.elements.loginpassword.value
         let isTrue = this.validate(loginusername,loginpassword);
         if(isTrue){
-            axios.post('/login',{
-                loginusername: loginusername,
-                loginpassword: loginpassword
+            axios({
+                method : 'post',
+                url : 'https://smart-chat-backend.herokuapp.com/login',
+                data : {
+                    loginusername: loginusername,
+                    loginpassword: loginpassword
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             })
             .then((res)=>{
                 if(res.data.isLoginSuccessful === true){
