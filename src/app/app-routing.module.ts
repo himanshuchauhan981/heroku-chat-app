c6b60b879dc4b194673efe63ec47a8fb000d12a8
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-02-13 14:05:22 +0530

    added new angular structure and removed old structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
-import { NgModule } from '@angular/core'
-import { Routes, RouterModule } from '@angular/router'
-import { LoginComponent } from './login/login.component'
-import { SignupComponent } from './signup/signup.component'
-import { HomeComponent } from './home/home.component'
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [
-	{ path: '', component: LoginComponent },
-	{ path: 'signup', component: SignupComponent },
-	{ path: 'login', component: LoginComponent},
-	{ path: 'home', component: HomeComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chatApp'`, () => {
+  it(`should have as title 'smart-chat-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('chatApp');
+    expect(app.title).toEqual('smart-chat-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('chatApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('smart-chat-app app is running!');
   });
 });
